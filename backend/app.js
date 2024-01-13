54cfccfe7fa53b8617651c817f54239453a862fe
Chirag2903 <chiragshukla135@gmail.com>
2024-01-13 22:04:58 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const app = express();
+
 
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config("/backend/.env");
+    require("dotenv").config();
 }
 
 
-const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 const user = require("../backend/routes/userroutes");
 app.use(user);
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"))
+})
 
 const connectdatabase = () => {
     mongoose.connect(process.env.mongoURI, {
@@ -26,11 +33,6 @@ connectdatabase();
 
 
 
-app.use(express.static(path.join(__dirname, "../build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"))
-})
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
