230e72281b89946c01cb6e4e02ba341a58b8d8e6
Chirag2903 <chiragshukla135@gmail.com>
2024-01-13 21:27:22 +0530

    update app

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+    require("dotenv").config({ path: "backend/.env" });
 }
 
 
